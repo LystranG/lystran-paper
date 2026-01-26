c8cf8ad1c56a5f76ac34cf6834f7f2ba813a8e0c
Lystran <me@lystran.com>
2026-01-26 21:03:07 +0800

    fix: partytown busuanzi

--- astro.config.ts
+++ astro.config.ts
@@ -52,18 +52,7 @@ export default defineConfig({
     id: "31c63cca-7a36-4dbe-a22b-9ed3377539b7",
     endpointUrl: "https://analytics.lystran.com",
     withPartytown: true
-  }), partytown({
-    config: {
-      resolveUrl: (url) => {
-
-        if (url.hostname === 'cdn.busuanzi.cc') {
-          return new URL('/_partytown-proxy/busuanzi' + url.pathname, location.href);
-        }
-        
-        return url;
-      }
-    }
-  })],
+  }), partytown()],
   markdown: {
     remarkPlugins: [
       remarkMath,
